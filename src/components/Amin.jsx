8c49f8cda3826acd4372e76a4e9a4a4d4ffdd57b
AzizBenIsmail <aziz.270700@gmail.com>
2025-09-07 12:24:10 +0100

    séance 1 react

---
+++ src/components/Amin.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+export default function amin() {
+    //logique
+    //js
+  return (    
+    <div>
+      {/* //vue
+      //html */}
+    </div>
+  )
+}
--- src/index.js
+++ src/index.js
@@ -3,11 +3,15 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import Navbar from './components/navbar/Navbar';
+import Carousel from "./components/Carousel/Carousel";
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
+    <Navbar/>
     <App />
+    <Carousel/>
   </React.StrictMode>
 );
 
